ce47d34ac0af03faa79b414dc7c763d2d9edd881
LeonardoMendes007 <mendesleo98@gmail.com>
2022-11-15 16:47:15 -0300

    first commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaginacaoApi.Data;
+using PaginacaoApi.Models;
+using System.Collections.Generic;
+
+namespace PaginacaoApi.Controllers
+{
+    [ApiController]
+    [Route("api/todo")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet(template:"load")]
+        public async Task<IActionResult> LoadAsync([FromServices] AppDbContext context)
+        {
+            for (int i = 0; i < 1500; i++)
+            {
+                var todo = new Todo()
+                {
+                    Id = i + 1,
+                    Done = true,
+                    CreatedAt = DateTime.Now,
+                    Title = $"Tarefa {i}"
+                };
+
+                await context.Todos.AddRangeAsync(todo);
+                await context.SaveChangesAsync();
+            }
+
+            return Ok("Load Successfully");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromServices] AppDbContext context,
+            [FromQuery] int skip = 0,
+            [FromQuery] int take = 25)
+        {
+            var total = await context.Todos.CountAsync();
+            var todos = await context
+                .Todos
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+
+            return Ok(new
+            {
+                total,
+                skip,
+                take,
+                data = todos
+            });
+        }
+    }
+}
